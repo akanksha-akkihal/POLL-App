bdf3c7d0345c1f709aad3a2b0fcf78a9f73b25d8
Akanksha27 <akankshaakkihal27@gmail.com>
2021-04-07 00:13:21 +0530

    add to firebase

--- app/src/main/java/com/example/pollingapp/createPoll.java
+++ app/src/main/java/com/example/pollingapp/createPoll.java
@@ -1,14 +1,67 @@
 package com.example.pollingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class createPoll extends AppCompatActivity {
+    Button create;
+    EditText question,option1,option2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_poll);
+
+        question= (EditText) findViewById(R.id.question);
+        option1 = (EditText) findViewById(R.id.option1);
+        option2 = (EditText) findViewById(R.id.option2);
+        create = (Button) findViewById(R.id.create);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addPoll();
+            }
+        });
+    }
+    private void addPoll(){
+        String question_string= question.getText().toString().trim();
+        String option1_string= option1.getText().toString().trim();
+        String option2_string= option2.getText().toString().trim();
+
+        String userId= FirebaseAuth.getInstance().getCurrentUser().getUid();
+        pollInfo newPoll=new pollInfo(question_string,option1_string,option2_string,userId);
+
+        FirebaseFirestore.getInstance()
+                .collection("questions")
+                .add(newPoll)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(createPoll.this,"Poll successfully created",Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(createPoll.this,e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
+
---
+++ app/src/main/java/com/example/pollingapp/pollInfo.java
@@ -0,0 +1,50 @@
+package com.example.pollingapp;
+
+public class pollInfo {
+    private String question;
+    private String option1;
+    private String option2;
+    private String userId;
+
+    public pollInfo() {
+    }
+
+    public pollInfo(String question, String option1, String option2, String userId) {
+        this.question = question;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.userId = userId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getOption1() {
+        return option1;
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
